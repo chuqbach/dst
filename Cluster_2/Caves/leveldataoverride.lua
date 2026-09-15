ae8183b8a22704d715afa34a86f6709614635c16
CuongPhan <cuongphan1408@gmail.com>
2026-09-16 00:10:24 +0700

    feat:day42 update

--- Cluster_2/Caves/leveldataoverride.lua
+++ Cluster_2/Caves/leveldataoverride.lua
@@ -119,7 +119,7 @@ return {
     weather="default",
     winters_feast="default",
     world_size="medium",
-    worldseed="",
+    worldseed=1743313427,
     wormattacks="default",
     wormattacks_boss="default",
     wormhole_prefab="tentacle_pillar",
